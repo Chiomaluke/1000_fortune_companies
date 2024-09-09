f065d804964284274183bde3a4a24ea46a2e98ca
ChiomaLuke <chiomaluke96@gmail.com>
2024-09-09 22:21:16 +0100

    deleted a file

--- hello.py
+++
@@ -1,9 +0,0 @@
-import numpy as np 
-import pandas as pd 
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-df = pd.read_csv(r'C:\Users\DELL\OneDrive\Desktop\data_analytics\fortune_1000_companies\fortune_companies.csv')
-
-df.head()
-
